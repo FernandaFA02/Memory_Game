8910020fde9d36ed8977a7058fd0b93677c20a29
FernandaFA02 <fajardo.al9402@gmail.com>
2021-11-01 15:07:40 -0600

    agregando transiciones

--- src/data.js
+++ src/data.js
@@ -1,13 +1,13 @@
-/*//Se trae la data del JSON con un fetch
+//Se trae la data del JSON con un fetch
 fetch("../Data/memory.json")
 .then((response) => response.json())
-.then((data) => iterarPersonajes(data))
+.then((data) => iterarTarjetas(data))
 .catch((error) => console.log(error))
 
 //Juego
 let memorama = document.getElementById("juego");
 //Se iteran el JSON de los personajes
-let iterarPersonajes = (data) => {
+let iterarTarjetas = (data) => {
     console.log(data.Memorama)
     for (const tarjeta of data.Memorama) {
         console.log(tarjeta.Personaje)
@@ -15,15 +15,13 @@ let iterarPersonajes = (data) => {
         <div class="memory-portrait">
         <div class="memory-cover" id='${tarjeta.Personaje}'></div>
         <div class="memory-back">
-            <h6>${tarjeta.Personaje}</h6>
+        <h6>${tarjeta.Personaje}</h6>
         </div>
         </div>
         </div>`;
     
-        let tarjetasLogo = document.getElementById(`${tarjeta.Personaje}`)
-                tarjetasLogo.style.backgroundImage = "url(" + `${tarjeta.Logo}` + ")"
-                tarjetasLogo.style.backgroundSize = "cover"
-
+        let portadaTarjeta = document.getElementById(`${tarjeta.Personaje}`)
+        portadaTarjeta.style.backgroundImage = "url(" + `${tarjeta.Logo}` + ")"
+        portadaTarjeta.style.backgroundSize = "cover"
     }
 }
-*/
